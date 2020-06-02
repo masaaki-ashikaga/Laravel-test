3df5ee815e21bb2c931a38289ba42b68b5b1bf61
masaaki-ashikaga <gamasaga0312@gmail.com>
2020-06-02 23:10:30 +0900

    クエリビルダshowページ

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -12,7 +12,8 @@ class HelloController extends Controller
 {
     public function index(Request $request)
     {
-        $items = DB::select('select * from people');
+        // $items = DB::select('select * from people');
+        $items = DB::table('people')->get();
         return view('hello.index', ['items' => $items]);
     }
 
@@ -20,6 +21,13 @@ class HelloController extends Controller
     {
         $items = DB::select('select * from people');
         return view('hello.index', ['items' => $items]);
+    }
+
+    public function show(Request $request)
+    {
+        $id = $request->id;
+        $item = DB::table('people')->where('id', $id)->first();
+        return view('hello.show', ['item' => $item]);
     }
 
     public function add(Request $request)
--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,7 @@ Route::get('hello/edit', 'HelloController@edit');
 Route::post('hello/edit', 'HelloController@update');
 Route::get('hello/del', 'HelloController@del');
 Route::post('hello/del', 'HelloController@remove');
+Route::get('hello/show', 'HelloController@show');
 
 
 Route::get('tests/test', 'TestController@index');
