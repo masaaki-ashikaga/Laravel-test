ab5238da0188dee67d0dac4785715ab62898e554
masaaki-ashikaga <gamasaga0312@gmail.com>
2020-06-02 22:51:13 +0900

    クエリビルダDELETE

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,8 @@ Route::get('hello/add', 'HelloController@add');
 Route::post('hello/add', 'HelloController@create');
 Route::get('hello/edit', 'HelloController@edit');
 Route::post('hello/edit', 'HelloController@update');
+Route::get('hello/del', 'HelloController@del');
+Route::post('hello/del', 'HelloController@remove');
 
 
 Route::get('tests/test', 'TestController@index');
